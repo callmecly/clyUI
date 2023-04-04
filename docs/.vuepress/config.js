1850545f630e6dbd51327e75d13732f22544d30e
chenly05 <chenly05@mingyuanyun.com>
2023-04-04 21:43:50 +0800

    feat: vue-cli降级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    title: 'cly-ui',
-    base: '/clyUI/',
-    themeConfig: {
-        nav: [
-            {
-                text: 'Home', link: '/'
-            },
-            {
-                text: "Github", link: 'https://github.com/callmecly/clyUI'
-            },
-            {
-                text: 'Npm', link: 'https://www.npmjs.com/package/cly-ui'
-            }
-        ],
-        sidebar: [
-            '/',
-            '/componentDocs/card'
-        ]
-    }
+  title: 'cly-ui',
+  base: '/clyUI/',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/callmecly/clyUI',
+      },
+      {
+        text: 'Npm',
+        link: 'https://www.npmjs.com/package/cly-ui',
+      },
+    ],
+    sidebar: ['/', '/componentDocs/card'],
+  },
 }
